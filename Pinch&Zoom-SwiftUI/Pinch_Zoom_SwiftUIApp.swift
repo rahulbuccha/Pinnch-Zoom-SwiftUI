7f644375fb1accd6786ae49a2f85e375c0e4f73e
Rahul Shaw <rahulbucha69@gmail.com>
2022-08-02 11:13:54 +0530

    Initial Commit

---
+++ Pinch&Zoom-SwiftUI/Pinch_Zoom_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_Zoom_SwiftUIApp.swift
+//  Pinch&Zoom-SwiftUI
+//
+//  Created by Admin on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_Zoom_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
